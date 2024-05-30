a6edbd1c686aa8f7afd0deea596c5550d0212c71
David H. Martin <dmartin.jcolonia@gmail.com>
2024-05-30 13:38:14 +0200

    Ampliación modelo tabla

--- 09.10 VentanaWorldBD (EX3B)/src/jcolonia/daw2023/junio/ModeloTablaPaíses.java
+++ 09.10 VentanaWorldBD (EX3B)/src/jcolonia/daw2023/junio/ModeloTablaPaíses.java
@@ -3,29 +3,26 @@ package jcolonia.daw2023.junio;
 import javax.swing.table.DefaultTableModel;
 
 class ModeloTablaPaíses extends DefaultTableModel {
-	private static final Class[] TIPO_COLUMNAS = { String.class, String.class,
-			String.class };
-	private static final boolean[] COLUMNAS_EDITABLES = { true, true, true };
-	private static final String[] NOMBRES_COLUMNA = { "País", "Capital",
-			"Idioma" };
+	private static final Class[] TIPOS_COLUMNAS = { Integer.class, String.class, String.class, String.class };
+	private static final boolean[] COLUMNAS_EDITABLES = { true, true, true, true };
+	private static final String[] NOMBRES_COLUMNAS = { "#", "País", "Capital", "Idioma" };
 
 	public ModeloTablaPaíses() {
-		super(NOMBRES_COLUMNA, 0);
+		super(NOMBRES_COLUMNAS, 0);
 	}
 
 	@Override
-	public Class<?> getColumnClass(int columnIndex) {
-		return TIPO_COLUMNAS[columnIndex];
+	public Class<?> getColumnClass(int columna) {
+		return TIPOS_COLUMNAS[columna];
 	}
 
 	@Override
-	public boolean isCellEditable(int row, int column) {
-		return COLUMNAS_EDITABLES[column];
+	public boolean isCellEditable(int fila, int columna) {
+		return COLUMNAS_EDITABLES[columna];
 	}
 
 	public void addRow(País nuevoPaís) {
-		addRow(new String[] { nuevoPaís.nombre(), nuevoPaís.capital(),
-				nuevoPaís.idioma() });
+		addRow(new Object[] { Integer.valueOf(0), nuevoPaís.nombre(), nuevoPaís.capital(), nuevoPaís.idioma() });
 	}
 
 	public void vaciar() {
